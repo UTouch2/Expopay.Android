0e1b955f2d60e1255b60819efa1389bcf8b7b000
tfs_xqk01 <xuqikang@expopay.cn>
2015-10-22 09:48:46 +0800

    create

--- Expopay.Android/app/src/main/java/com/expopay/android/model/CompanyEntity.java
+++ Expopay.Android/app/src/main/java/com/expopay/android/model/CompanyEntity.java
@@ -1,9 +1,11 @@
 package com.expopay.android.model;
 
+import java.io.Serializable;
+
 /**
  * Created by misxu012 on 2015/10/21.
  */
-public class CompanyEntity {
+public class CompanyEntity  implements Serializable{
     String  companyName;
 
     public String getCompanyName() {
