b2d655e54b4af3693ef6405b5e9bc61efcbd2fea
tfs_xqk01 <xuqikang@expopay.cn>
2015-10-23 15:48:50 +0800

    upd

---
+++ Expopay.Android/app/src/main/java/com/expopay/android/model/AddressEntity.java
@@ -0,0 +1,94 @@
+package com.expopay.android.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by misxu012 on 2015/10/23.
+ */
+public class AddressEntity implements Serializable {
+
+    private String addressId;
+    private String personName;
+    private String mobile;
+    private String provinceName;
+    private String cityName;
+    private String districtName;
+    private String address;
+    private String ZipCode;
+    private String isDefault;
+
+    public AddressEntity() {
+    }
+
+    public String getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(String addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getDistrictName() {
+        return districtName;
+    }
+
+    public void setDistrictName(String districtName) {
+        this.districtName = districtName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getZipCode() {
+        return ZipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        ZipCode = zipCode;
+    }
+
+    public String getIsDefault() {
+        return isDefault;
+    }
+
+    public void setIsDefault(String isDefault) {
+        this.isDefault = isDefault;
+    }
+}
