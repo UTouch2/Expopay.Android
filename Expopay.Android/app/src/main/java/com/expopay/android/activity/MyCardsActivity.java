bf1ffbe06fba63caa0de2c3edc19bc6f4f7c5957
tfs_xqk01 <xuqikang@expopay.cn>
2015-10-23 15:49:10 +0800

    upd

--- Expopay.Android/app/src/main/java/com/expopay/android/activity/MyCardsActivity.java
+++ Expopay.Android/app/src/main/java/com/expopay/android/activity/MyCardsActivity.java
@@ -1,22 +1,32 @@
 package com.expopay.android.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.expopay.android.R;
+import com.expopay.android.adapter.listview.MyCardsListAdapter;
+import com.expopay.android.model.CardEntity;
 import com.expopay.android.request.CardRequest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by misxu012 on 2015/10/22.
  */
 public class MyCardsActivity extends BaseActivity {
     ListView listView;
+    List<CardEntity> data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mycards);
         listView =(ListView)findViewById(R.id.mycards_listview);
+        data = testDate();
+        listView.setAdapter(new MyCardsListAdapter(getApplicationContext(),data));
     }
     public void lossOnclick(View v){
 
@@ -26,5 +36,17 @@ public class MyCardsActivity extends BaseActivity {
     protected void onResume() {
         super.onResume();
     }
-    CardRequest request = new CardRequest("");
+
+    private  List<CardEntity>  testDate(){
+        List<CardEntity> list = new ArrayList<CardEntity>();
+        for(int i = 0;i<10;i++){
+            CardEntity  e =new CardEntity();
+            e.setBalance("20");
+            e.setCardType("qweqweq");
+            e.setIsDefault("0");
+            e.setCardNumber("88888888888888888");
+            list.add(e);
+        }
+        return list;
+    }
 }
--- Expopay.Android/app/src/main/java/com/expopay/android/activity/SettingsActivity.java
+++ Expopay.Android/app/src/main/java/com/expopay/android/activity/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.expopay.android.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,8 +10,6 @@ import com.expopay.android.R;
  * Created by misxu012 on 2015/10/22.
  */
 public class SettingsActivity extends BaseActivity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,20 +17,31 @@ public class SettingsActivity extends BaseActivity {
     }
 
     public void changeMobileOnclick(View v) {
-
+        Intent intent =new Intent(getApplicationContext(),ChangeMobileActivity.class);
+        startActivity(intent);
     }
-
-
     public void changeLoginPasswordOnclock(View v) {
+        Intent intent =new Intent(getApplicationContext(),ChangePasswordActivity.class);
+        startActivity(intent);
     }
-
     public void changePayPasswordOnclock(View v) {
-
+        Intent intent =new Intent(getApplicationContext(),ChangePayPasswordActivity.class);
+        startActivity(intent);
     }
     public void changePasswordQuestionOnclick(View v) {
-
+        Intent intent =new Intent(getApplicationContext(),ChangePasswordQuestionAcitivy.class);
+        startActivity(intent);
     }
     public void autoPayOnclick(View v) {
+        Intent intent =new Intent(getApplicationContext(),ChangePasswordActivity.class);
+        startActivity(intent);
+    }
+
+    public void addressOnclick(View v){
+
+    }
+
+    public void cardPasswordOnclock(View v){
 
     }
 }
