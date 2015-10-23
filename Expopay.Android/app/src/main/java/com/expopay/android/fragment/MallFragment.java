ebf5c38cb89ba39f68a580706ea88fe921b37c69
UTouch2 <mengbochun@expopay.cn>
2015-10-23 12:38:58 +0800

    update

--- Expopay.Android/app/src/main/java/com/expopay/android/fragment/MallFragment.java
+++ Expopay.Android/app/src/main/java/com/expopay/android/fragment/MallFragment.java
@@ -1,5 +1,6 @@
 package com.expopay.android.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.GridView;
 
 import com.expopay.android.R;
+import com.expopay.android.activity.ProductDetailsActivity;
 import com.expopay.android.adapter.gridview.MallProductAdapter;
 import com.expopay.android.model.MallProductEntity;
 
@@ -23,7 +25,15 @@ public class MallFragment extends  BaseFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view =inflater.inflate(R.layout.fragment_mall,null,false);
+        View view =inflater.inflate(R.layout.fragment_mall,container,false);
+
+        view.findViewById(R.id.imageButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ProductDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
 
 //        myGridView = (GridView) view.findViewById(R.id.mygridview);
 //        adapter = new MallProductAdapter(getActivity().getApplicationContext(),list);
