893960e745ffe81fbf087d05ed2f25f960eb25fb
tfs_xqk01 <xuqikang@expopay.cn>
2015-10-23 16:03:36 +0800

    Merge branch 'master' of http://10.66.2.12:8080/tfs/Product/Expopay/_git/App.Android.Customer
    Conflicts:
    	Expopay.Android/app/src/main/res/layout/activity_order_detail.xml

--- Expopay.Android/app/src/main/java/com/expopay/android/activity/MyAddressActivity.java
+++ Expopay.Android/app/src/main/java/com/expopay/android/activity/MyAddressActivity.java
@@ -4,18 +4,39 @@ import android.os.Bundle;
 import android.widget.ListView;
 
 import com.expopay.android.R;
+import com.expopay.android.adapter.listview.MyAddressListAdapter;
+import com.expopay.android.model.AddressEntity;
+import com.expopay.android.model.CardEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by misxu012 on 2015/10/23.
  */
 public class MyAddressActivity extends BaseActivity {
     ListView listView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myaddress);
         listView = (ListView) findViewById(R.id.myaddress_listview);
+        listView.setAdapter(new MyAddressListAdapter(getApplicationContext(),testDate()));
     }
-
+    private List<AddressEntity> testDate(){
+        List<AddressEntity> list = new ArrayList<AddressEntity>();
+        for(int i = 0;i<10;i++){
+            AddressEntity  e =new AddressEntity();
+            e.setIsDefault("0");
+            e.setAddress("qwertyuio;sdfghjkl;asdvabsdaosda[sl,[dals,");
+            e.setAddressId("" + i);
+            e.setCityName("昆明市");
+            e.setDistrictName("五华区");
+            e.setMobile("13245678901");
+            e.setPersonName("克里斯");
+            e.setProvinceName("云南省");
+            e.setZipCode("234567890");
+        }
+        return list;
+    }
 }
