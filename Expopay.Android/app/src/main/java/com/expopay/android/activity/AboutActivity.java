5b9538de9e1afe9c810ffd1857cc4e3252bb7136
tfs_xqk01 <xuqikang@expopay.cn>
2015-10-23 12:47:46 +0800

    upd

---
+++ Expopay.Android/app/src/main/java/com/expopay/android/activity/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.expopay.android.activity;
+
+import android.os.Bundle;
+
+import com.expopay.android.R;
+
+/**
+ * Created by misxu012 on 2015/10/23.
+ */
+public class AboutActivity extends  BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
---
+++ Expopay.Android/app/src/main/java/com/expopay/android/activity/MyBillsActivity.java
@@ -0,0 +1,13 @@
+package com.expopay.android.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by misxu012 on 2015/10/23.
+ */
+public class MyBillsActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
