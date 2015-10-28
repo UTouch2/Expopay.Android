9638de4645868e6135c5293dab7777c611d34492
tfs_xqk01 <xuqikang@expopay.cn>
2015-10-28 13:40:54 +0800

    upd

--- Expopay.Android/app/src/main/java/com/expopay/android/activity/WebActivity.java
+++ Expopay.Android/app/src/main/java/com/expopay/android/activity/WebActivity.java
@@ -11,6 +11,7 @@ public class WebActivity extends BaseWebActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setStatusColor();
         addJavascriptInterface(new MyJavascriptInterace());
     }
 
