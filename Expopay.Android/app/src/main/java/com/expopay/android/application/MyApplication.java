eec6917066ce81f07e4d6ab77d7c3d838c8a156c
tfs_xqk01 <xuqikang@expopay.cn>
2015-10-29 15:23:24 +0800

    upd

--- Expopay.Android/app/src/main/java/com/expopay/android/application/MyApplication.java
+++ Expopay.Android/app/src/main/java/com/expopay/android/application/MyApplication.java
@@ -1,16 +1,12 @@
 package com.expopay.android.application;
-
 import android.app.Application;
-
 /**
  * Created by misxu012 on 2015/10/20.
  */
 public class MyApplication extends Application {
     public final static String HOST = "";
-
     public static final String PHONE = "400-022-7299";
     public static final String EMAIL = "service@expopay.cn";
     public static final String NET = "http://www.expopay.cn";
-
     public final static String DB_NAME = "expopay.db";
 }
