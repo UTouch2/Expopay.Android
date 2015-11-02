4b6f4f2f228d2aa6ef76b353c84fa8a459ff5028
tfs_xqk01 <xuqikang@expopay.cn>
2015-11-02 13:24:07 +0800

    create

--- Expopay.Android/app/src/main/java/com/expopay/android/activity/AboutActivity.java
+++ Expopay.Android/app/src/main/java/com/expopay/android/activity/AboutActivity.java
@@ -11,7 +11,7 @@ import com.expopay.android.application.MyApplication;
 /**
  * Created by misxu012 on 2015/10/23.
  */
-public class AboutActivity extends BaseActivity {
+public class  AboutActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
