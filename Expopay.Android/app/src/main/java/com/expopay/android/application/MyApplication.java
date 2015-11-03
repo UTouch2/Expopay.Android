1d7d0eb06bc002bc691dfcc51c10499bbc86029a
tfs_xqk01 <xuqikang@expopay.cn>
2015-11-03 09:12:57 +0800

    create

--- Expopay.Android/app/src/main/java/com/expopay/android/application/MyApplication.java
+++ Expopay.Android/app/src/main/java/com/expopay/android/application/MyApplication.java
@@ -11,8 +11,4 @@ public class MyApplication extends Application {
     public static final String EMAIL = "service@expopay.cn";
     public static final String NET = "http://www.expopay.cn";
     public final static String DB_NAME = "expopay.db";
-    public final static String USERNAME = "username";
-    public final static String PASSWORD = "password";
-    ;
-    public final static String OPENID = "openId";
 }
