434d20c5165da3c3ca348693dcf4719c415f45be
tfs_xqk01 <xuqikang@expopay.cn>
2015-11-04 15:34:53 +0800

    update

--- Expopay.Android/app/src/main/java/com/expopay/android/view/CustormViewPager.java
+++ Expopay.Android/app/src/main/java/com/expopay/android/view/CustormViewPager.java
@@ -59,7 +59,6 @@ public class CustormViewPager extends ViewPager {
 
         @Override
         public void startScroll(int startX, int startY, int dx, int dy) {
-            // Ignore received duration, use fixed one instead
             super.startScroll(startX, startY, dx, dy, mDuration);
         }
 
@@ -71,4 +70,5 @@ public class CustormViewPager extends ViewPager {
             return mDuration;
         }
     }
+
 }
