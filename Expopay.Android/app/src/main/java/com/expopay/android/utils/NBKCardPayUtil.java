78290aee7698f5fa50d5a0b1cbd18e20da830d04
tfs_xqk01 <xuqikang@expopay.cn>
2015-11-24 10:09:41 +0800

    upd

--- Expopay.Android/app/src/main/java/com/expopay/android/utils/NBKCardPayUtil.java
+++ Expopay.Android/app/src/main/java/com/expopay/android/utils/NBKCardPayUtil.java
@@ -4,12 +4,15 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 
+import com.expopay.android.activity.BaseActivity;
 import com.expopay.android.activity.NBCardPayActivity;
+
 /**
  * Created by misxu012 on 2015/11/4.
  */
 public class NBKCardPayUtil {
     public static final int PAY_REQUEST_CODE = 99;
+
     /**
      * @param activity
      * @param orderNumer
@@ -17,10 +20,17 @@ public class NBKCardPayUtil {
      * @param orderAmount
      */
     public static void nbkCardPay(Activity activity, String orderNumer, String orderSource, String orderAmount) {
-        Intent intent = new Intent(activity, NBCardPayActivity.class);
-        intent.putExtra("orderNumber", orderNumer);
-        intent.putExtra("orderSource", orderSource);
-        intent.putExtra("orderAmount", orderAmount);
-        activity.startActivityForResult(intent, PAY_REQUEST_CODE);
+        String payStatus = ((BaseActivity) activity).getUser().getPayStatus();
+        // 设置支付密码页面
+        if ("0".equals(payStatus)) {
+
+        } else if ("1".equals(payStatus)) {
+            Intent intent = new Intent(activity, NBCardPayActivity.class);
+            intent.putExtra("orderNumber", orderNumer);
+            intent.putExtra("orderSource", orderSource);
+            intent.putExtra("orderAmount", orderAmount);
+            activity.startActivityForResult(intent, PAY_REQUEST_CODE);
+        }
+
     }
 }
