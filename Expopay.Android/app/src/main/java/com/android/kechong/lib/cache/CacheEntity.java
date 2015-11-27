b256e202cf09f1cac3609c425d6239eef91def4e
tfs_xqk01 <xuqikang@expopay.cn>
2015-11-27 10:02:36 +0800

    Merge branch 'master' of http://10.66.2.12:8080/tfs/Product/Expopay/_git/App.Android.Customer
    Conflicts:
    	Expopay.Android/app/src/main/res/layout/activity_produte_details.xml

--- Expopay.Android/app/src/main/java/com/android/kechong/lib/cache/CacheEntity.java
+++ Expopay.Android/app/src/main/java/com/android/kechong/lib/cache/CacheEntity.java
@@ -5,68 +5,68 @@ import java.io.Serializable;
 import android.graphics.Bitmap;
 
 public class CacheEntity implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private Long time;
-	private Object content;
-	private String stringContent;
-	private Bitmap bitmapContent;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private Long time;
+    private Object content;
+    private String stringContent;
+    private Bitmap bitmapContent;
 
-	public CacheEntity() {
-		super();
-	}
+    public CacheEntity() {
+        super();
+    }
 
-	public Long getTime() {
-		return time;
-	}
+    public Long getTime() {
+        return time;
+    }
 
-	public void setTime(Long time) {
-		this.time = time;
-	}
+    public void setTime(Long time) {
+        this.time = time;
+    }
 
-	public String getStringContent() {
-		if (content instanceof String) {
-			return (String) content;
-		}
-		return null;
-	}
+    public String getStringContent() {
+        if (content instanceof String) {
+            return (String) content;
+        }
+        return null;
+    }
 
-	public void setStringContent(String stringContent) {
-		this.stringContent = stringContent;
-	}
+    public void setStringContent(String stringContent) {
+        this.stringContent = stringContent;
+    }
 
-	public Bitmap getBitmapContent() {
-		if (content instanceof Bitmap) {
-			return (Bitmap) content;
-		}
-		return null;
-	}
+    public Bitmap getBitmapContent() {
+        if (content instanceof Bitmap) {
+            return (Bitmap) content;
+        }
+        return null;
+    }
 
-	public void setBitmapContent(Bitmap bitmapContent) {
-		this.bitmapContent = bitmapContent;
-	}
+    public void setBitmapContent(Bitmap bitmapContent) {
+        this.bitmapContent = bitmapContent;
+    }
 
-	public Object getContent() {
-		return content;
-	}
+    public Object getContent() {
+        return content;
+    }
 
-	public void setContent(Object content) {
-		this.content = content;
-	}
+    public void setContent(Object content) {
+        this.content = content;
+    }
 
-	public int sizeOf() {
-		int size = 0;
-		if (time != null) {
-			size += time.toString().length();
-		}
-		if (stringContent != null) {
-			size += stringContent.toString().length();
-		}
-		if (bitmapContent != null) {
-			size += bitmapContent.toString().length();
-		}
-		return size;
-	}
+    public int sizeOf() {
+        int size = 0;
+        if (time != null) {
+            size += time.toString().length();
+        }
+        if (stringContent != null) {
+            size += stringContent.toString().length();
+        }
+        if (bitmapContent != null) {
+            size += bitmapContent.toString().length();
+        }
+        return size;
+    }
 }
