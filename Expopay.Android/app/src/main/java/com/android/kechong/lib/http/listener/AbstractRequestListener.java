c4a8a8869079f38591a768272ee521417aaff711
tfs_xqk01 <xuqikang@expopay.cn>
2015-11-27 14:43:47 +0800

    Merge branch 'master' of http://10.66.2.12:8080/tfs/Product/Expopay/_git/App.Android.Customer
    Conflicts:
    	Expopay.Android/app/src/main/res/layout/activity_produte_details.xml

--- Expopay.Android/app/src/main/java/com/android/kechong/lib/http/listener/AbstractRequestListener.java
+++ Expopay.Android/app/src/main/java/com/android/kechong/lib/http/listener/AbstractRequestListener.java
@@ -1,38 +1,36 @@
 package com.android.kechong.lib.http.listener;
 
-import com.android.kechong.lib.cache.manager.AbsFileCacheManager;
-import com.android.kechong.lib.cache.manager.AbsMemoryCacheManager;
 import com.android.kechong.lib.http.Exception.RequestInterruptedException;
 
 public abstract class AbstractRequestListener implements com.android.kechong.lib.http.listener.IRequestListener {
 
-	/**
-	 * 完成
-	 */
-	public static final int FINISH = 1;
-	/**
-	 * 正在下载
-	 */
-	public static final int DOING = 2;
-	public boolean isCancelled;
-	protected AbsMemoryCacheManager memoryCache;
-	protected AbsFileCacheManager fileCache;
+    /**
+     * 完成
+     */
+    public static final int FINISH = 1;
+    /**
+     * 正在下载
+     */
+    public static final int DOING = 2;
+    public boolean isCancelled;
 
-	public AbstractRequestListener() {
-		super();
-	}
-	@Override
-	public void cancel() {
-		isCancelled = true;
-	}
-	@Override
-	public void checkIfCanceled() throws RequestInterruptedException {
-		if (isCancelled) {
-			throw new RequestInterruptedException("request has been cancelled");
-		}
-	}
+    public AbstractRequestListener() {
+        super();
+    }
 
-	public Integer[] getProgress() {
-		return null;
-	}
+    @Override
+    public void cancel() {
+        isCancelled = true;
+    }
+
+    @Override
+    public void checkIfCanceled() throws RequestInterruptedException {
+        if (isCancelled) {
+            throw new RequestInterruptedException("request has been cancelled");
+        }
+    }
+
+    public Integer[] getProgress() {
+        return null;
+    }
 }
