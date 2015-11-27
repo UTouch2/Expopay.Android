58c7cb9864d9b9382c7e28167414f4852acc5837
tfs_xqk01 <xuqikang@expopay.cn>
2015-11-27 10:02:12 +0800

    Merge branch 'master' of http://10.66.2.12:8080/tfs/Product/Expopay/_git/App.Android.Customer
    Conflicts:
    	Expopay.Android/app/src/main/res/layout/activity_produte_details.xml

--- Expopay.Android/app/src/main/java/com/expopay/android/application/MyApplication.java
+++ Expopay.Android/app/src/main/java/com/expopay/android/application/MyApplication.java
@@ -2,13 +2,17 @@ package com.expopay.android.application;
 
 import android.app.Application;
 
+import com.android.kechong.lib.BaseApplication;
+import com.android.kechong.lib.cache.manager.BitmapMemoryCacheManager;
+
 /**
  * Created by misxu012 on 2015/10/20.
  */
-public class MyApplication extends Application {
+public class MyApplication extends BaseApplication {
     public final static String HOST = "http://10.66.4.75:5391/";
     public static final String PHONE = "400-022-7299";
     public static final String EMAIL = "service@expopay.cn";
     public static final String NET = "http://www.expopay.cn";
     public final static String DB_NAME = "expopay.db";
+    public static BitmapMemoryCacheManager bitmapMemoryCacheManager = BitmapMemoryCacheManager.getInstance();
 }
